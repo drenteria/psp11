4eb7561a443326487961fdfc39ea71703293d06c
Daniel Renteria <df.renteria10@uniandes.edu.co>
2015-02-18 21:26:28 -0500

    PSP11: v0.3 Vista de Web OK

--- src/main/java/edu/uniandes/ecos/psp11/controller/WebController.java
+++ src/main/java/edu/uniandes/ecos/psp11/controller/WebController.java
@@ -1,10 +1,77 @@
 package edu.uniandes.ecos.psp11.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Hashtable;
+import java.util.LinkedList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.uniandes.ecos.psp11.model.RelativeSizeCalculator;
+
 /**
  * This class is the controller for the web view
  * @author drenteria
  *
  */
 public class WebController {
+	
+	/**
+	 * Show main input form for the input list for Relative Size
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 */
+	public static void showInputForm(HttpServletRequest request, HttpServletResponse response) 
+	throws IOException{
+		String formString = "<html>" +
+				"<body>" +
+				"<h1>PSP11 Relative Size Estimations Program!</h1>" +
+				"<p>Please, write numbers to calculate in semicolon (;) separated values.</p>" +
+				"<form action=\"relative\" method=\"post\"><br/>" +
+				"Input Data: <input type=\"text\" name=\"list\"><br/>" +
+				"<input type=\"submit\" value=\"Show Relative Sizes\">" +
+				"</body>" +
+				"</html>";
+		PrintWriter writer = response.getWriter();
+		writer.write(formString);
+	}
+	
+	/**
+	 * Show results 
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 */
+	public static void showRelativeSizesFromInput(HttpServletRequest request, HttpServletResponse response, 
+			LinkedList<Double> values, RelativeSizeCalculator theCalc)
+	throws IOException{
+		
+		Hashtable<String, Double> relativeSizes = new Hashtable<String, Double>();
+		relativeSizes = theCalc.calculateRelativeSizeRanges(values);
+		
+		System.out.println("Input Data: " + values.toString());
+		System.out.println("Very Small: " + relativeSizes.get(RelativeSizeCalculator.VS));
+		System.out.println("Small: " + relativeSizes.get(RelativeSizeCalculator.S));
+		System.out.println("Medium: " + relativeSizes.get(RelativeSizeCalculator.M));
+		System.out.println("Large: " + relativeSizes.get(RelativeSizeCalculator.L));
+		System.out.println("Very Large: " + relativeSizes.get(RelativeSizeCalculator.VL));
+		
+		String resultString = "<html>" +
+				"<body>" +
+				"<h1>PSP11 Relative Size Estimations Program!</h1>" +
+				"<p>Results:</p>" +
+				"Inputs :" + values.toString() + "<br/>" +
+				"Very Small: " + relativeSizes.get(RelativeSizeCalculator.VS) + "<br/>" +
+				"Small: " + relativeSizes.get(RelativeSizeCalculator.S) + "<br/>" +
+				"Medium: " + relativeSizes.get(RelativeSizeCalculator.M) + "<br/>" +
+				"Large: " + relativeSizes.get(RelativeSizeCalculator.L) + "<br/>" +
+				"Very Large: " + relativeSizes.get(RelativeSizeCalculator.VL) + "<br/>" +
+				"</body>" +
+				"</html>";
+		PrintWriter writer = response.getWriter();
+		writer.write(resultString);
+	}
 
 }
--- src/main/java/edu/uniandes/ecos/psp11/view/App.java
+++ src/main/java/edu/uniandes/ecos/psp11/view/App.java
@@ -1,13 +1,74 @@
 package edu.uniandes.ecos.psp11.view;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import edu.uniandes.ecos.psp11.controller.WebController;
+import edu.uniandes.ecos.psp11.model.RelativeSizeCalculator;
+
+
 /**
- * Hello world!
+ * Web View!
  *
  */
-public class App 
+public class App extends HttpServlet
 {
-    public static void main( String[] args )
+	private static final long serialVersionUID = 1L;
+
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new App()), "/*");
+        try {
+            server.start();
+            server.join();
+        } catch (Exception ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+		try {
+			WebController.showInputForm(req, resp);
+		} catch (IOException e) {
+			Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, e);
+			e.printStackTrace();
+		}
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp){
+		String entryValues = req.getParameter("list");
+		
+		try{
+			LinkedList<Double> values = new LinkedList<Double>();
+			
+			
+			String[] inValues = entryValues.split(";");
+			for(String currentVal : inValues){
+				values.add(Double.valueOf(currentVal.trim()));
+			}
+			RelativeSizeCalculator theCalc = new RelativeSizeCalculator();
+			WebController.showRelativeSizesFromInput(req, resp, values, theCalc);
+			
+		}
+		catch (Exception ex){
+			Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+			ex.printStackTrace();
+		}
+		
+	}
 }
