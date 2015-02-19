8fd19d049de33d93b0747c7c02c73e2933d85efc
Daniel Renteria <df.renteria10@uniandes.edu.co>
2015-02-18 19:54:39 -0500

    PSP11: v0.0 Esqueleto Maven

---
+++ src/main/java/edu/uniandes/ecos/psp11/view/App.java
@@ -0,0 +1,13 @@
+package edu.uniandes.ecos.psp11.view;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
